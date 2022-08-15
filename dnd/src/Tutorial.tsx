45e5b80d024aee335e026a470f5982be247ce600
charles-afk <agudelo.charles@gmail.com>
2022-08-15 04:59:01 -0400

    draggable

--- dnd/src/Tutorial.tsx
+++ dnd/src/Tutorial.tsx
@@ -1,13 +1,14 @@
 // @ts-nocheck
 import {useState} from 'react'
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
 
 const Tutorial = () => {
     const config = {
         tabs: {
-            'snapchat': {id:1, visible:true, extra:false, order:1, name:'snapchat'},
-            'youtube': {id:2, visible:true, extra:false, order:2, name:'youtube'},
-            'instagram': {id:3, visible:true, extra:false, order:3, name:'instagram'},
-            'facebook': {id: 4, visible:true, extra:true, order:4, name:'facebook'}
+            'snapchat': {id:'1', visible:true, extra:false, order:1, name:'snapchat'},
+            'youtube': {id:'2', visible:true, extra:false, order:2, name:'youtube'},
+            'instagram': {id:'3', visible:true, extra:false, order:3, name:'instagram'},
+            'facebook': {id:'4', visible:true, extra:true, order:4, name:'facebook'}
         },
         rows: {
             'row-1': {
@@ -25,26 +26,60 @@ const Tutorial = () => {
         
     }
 
+    const listContainer = {
+        margin:8,border:'1px solid lightgrey',
+        borderRadius:2,
+        display:'flex',
+        flexDirection:'row',
+        alignItems:'center'
+    }
+
+    const tabList = {
+        display:'flex',
+        flexDirection:'row',
+    }
+
+    const tabContainer = {
+        margin:8,
+        border:'3px solid lightgrey',
+        borderRadius:2,
+        padding:8
+    }
+
     const [state, setState] = useState(config);
 
+    const onDragEnd = result => {
+        // TODO: reorder our column
+    };
+
     return (
-        state.rowOrder.map((rowName) => {
-            const row = state.rows[rowName]
-            const tabs = row.tabNames.map(name => state.tabs[name])
-            
-            return <div key={row.id} style={{margin:8,border:'1px solid lightgrey',borderRadius:2}}>
-                <h3 style={{padding:8}}>
-                    {row.title}
-                </h3>
-                {tabs.map(tab => {
-                    return <div key={tab.id} >
-                        <div style={{margin:8,border:'1px solid lightgrey',borderRadius:2,padding:8}}>
-                            {tab.name}
+        <DragDropContext onDragEnd={onDragEnd}>
+            {state.rowOrder.map( (rowName) => {
+                const row = state.rows[rowName]
+                const tabs = row.tabNames.map(name => state.tabs[name])
+                
+                return <Droppable key={row.id} droppableId={row.id} > 
+                    { (provided, snapshot) => ( 
+                        <div ref={provided.innerRef} {...provided.droppableProps} style={listContainer}>
+                            <h3 style={{padding:8}}>
+                                {row.title}
+                            </h3> 
+                            {tabs.map((tab,index) => (
+                                <Draggable key={tab.id} draggableId={tab.id} index={index}>
+                                    {(provided, snapshot) => (
+                                        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                            <div style={tabContainer}>
+                                                {tab.name}
+                                            </div>
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
                         </div>
-                    </div>;
-                })}
-            </div>;
-        })
+                    )}
+                </Droppable>;
+            })}
+        </DragDropContext>
     )
 }
 
