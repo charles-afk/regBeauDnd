3e68a884fb6c93dc252f61f387e3f0201ab7b9ad
charles-afk <agudelo.charles@gmail.com>
2022-09-27 00:11:54 -0400

    adding dnd kit

---
+++ dnd/src/dndKit.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DndContext, useDroppable, useDraggable } from '@dnd-kit/core';
+import { CSS } from '@dnd-kit/utilities';
+
+const dndKit = () => {
+    return (
+        <div>
+            dndKit
+        </div>
+    )
+}
+
+export default dndKit
